f9a3483b6dbd5c7b0a33e2fe107018c7a3121aef
keipro00 <keipro00@outlook.jp>
2020-11-12 22:55:22 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,8 +35,11 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: "DESC")
   end
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    # Post.create(content: params[:content])
+    # redirect_to action: :index
+    # レスポンスをJSONへ変更
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
   def checked
     
